0024c3dabd8fd721b3d5b22991190157cc075714
Areta Santos <aretasantoss@gmail.com>
2021-03-11 19:18:21 +0200

    tests

--- src/test/java/com/example/Bookstore/BookRespositoryTest.java
+++ src/test/java/com/example/Bookstore/BookRespositoryTest.java
@@ -2,36 +2,38 @@ package com.example.Bookstore;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.awt.print.Book;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import com.example.Bookstore.web.Book;
+import com.example.Bookstore.web.BookRepository;
+import com.example.Bookstore.web.Category;
 
-import com.example.Bookstore.web.*;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
-
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 public class BookRespositoryTest {
-	
+
 	@Autowired
 	private BookRepository repository;
 
-	@Test 
-	public void findByAuthorSHouldRetrunBook() {
-	      List<Book> books = repository.findByTitle("It");
-	        
-	        assertThat(books).hasSize(1);
-	        assertThat(books.get(0).getauthor()).isEqualTo("Stephen King");
-	    }
-	    
-	    @Test
-	    public void createNewBook() {
-	    	Book book = new Book("The outsider", "Stephen King", 2018, "25656", "29,90€", new Category("Scifi/horror"));
-	    	repository.save(book);
-	    	assertThat(book.getId()).isNotNull();
-	    }    
+	@Test
+	public void findByAuthorShouldRetrunBook() {
+		List<Book> books = repository.findByTitle("It");
 
+		assertThat(books).hasSize(1);
+		assertThat(books.get(0).getAuthor()).isEqualTo("Stephen King");
 	}
+
+	@Test
+	public void createNewBook() {
+		Book book = new Book("The outsider", "Stephen King", 2018, "25656", "29,90€", new Category("Scifi"));
+		repository.save(book);
+		assertThat(book.getId()).isNotNull();
+	}
+
+}
